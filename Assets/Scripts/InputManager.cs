e3428b03f6bd76d400436f8e14db7e0716e2d647
Seung Gyun Lee <stbluedevil63@gmail.com>
2015-03-05 02:56:38 +0900

    20150305

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour {
+    public GameObject player;
+    public float moveSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        PlayerControl();
+	
+	}
+
+    void PlayerControl()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.Translate(0.0f, 0.0f, Time.deltaTime * 4.0f);
+            Debug.Log("w");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Translate(Time.deltaTime * -4.0f, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.Translate(0.0f, 0.0f, Time.deltaTime * -4.0f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Translate(Time.deltaTime * 4.0f, 0.0f, 0.0f);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/SmoothFollowCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollowCamera : MonoBehaviour {
+
+   // public float smooth = 1.0f;
+    Transform _transform;
+    public Transform targetObject;
+    public float Height = 3.0f;
+    public float dist = 5.0f;
+    public float heightDamping = 2.0f;
+    public float targtYPos = 2.0f;
+    Vector3 lookAtTarget;
+	// Use this for initialization
+    void Awake()
+    {
+        _transform = transform;
+     
+        
+    }
+
+	void LateUpdate()
+    {
+        if (targetObject == null)
+            return;
+        float wantedHeight = targetObject.position.y + Height;
+        float currentHeight = transform.position.y;
+
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+        //x-z평면에 대한 거리를 캐릭터 기준 뒤로 이동
+        _transform.position = targetObject.position;
+        _transform.position -= Vector3.forward * dist;
+
+        //카메라 높이 설정
+        Vector3 cameraPos = _transform.position;
+        cameraPos.y = currentHeight;
+        _transform.position = cameraPos;
+        //카메라가 타겟을 보게 설정
+        Vector3 lookAtTarget = targetObject.position;
+        lookAtTarget.y = lookAtTarget.y + targtYPos;
+        
+        transform.LookAt(lookAtTarget);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
